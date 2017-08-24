e1450b63caea4844038247a9142bb636ca1caa58
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2017-08-24 22:49:19 +0200

    liczniki

--- src/App.js
+++ src/App.js
@@ -14,41 +14,42 @@ const counter2$ = new ValueSubject(1);
 const char$ = counter2$.asObservable().map(count => '.'.repeat(Math.min(count, 30)));
 
 setInterval(() => {
-	counter$.next(counter$.getValue() + 1);
+    counter$.next(counter$.getValue() + 1);
 }, 2000);
 
 setInterval(() => {
-	counter2$.next(counter2$.getValue() + 1);
+    counter2$.next(counter2$.getValue() + 1);
 }, 3000);
 
 type PropsType = {
     messageId: string,
 };
 
-class MessageItem extends BaseComponent<PropsType> {                        //dziedziczenie dostarcza typy - dekorator funkcjonalność
+							//dziedziczenie dostarcza funkcję getValue$ - która mapuje obserwator na wartosć
+class MessageItem extends BaseComponent<PropsType> {
     render() {
         const { messageId } = this.props;
         const counter = this.getValue$(counter$.asObservable());
-		const char = this.getValue$(char$);
+        const char = this.getValue$(char$);
 
-		return (
-			<div>
-				<p>model tenże { messageId } { counter }</p>
-				<p>==> {char}</p>
-			</div>
-		);
+        return (
+            <div>
+                <p>model tenże { messageId } { counter }</p>
+                <p>==> {char}</p>
+            </div>
+        );
     }
 }
 
 
 class App extends React.Component<{}> {
-	render() {
-		return (
-			<div className="App">
-				<MessageItem messageId="aaazzzddd1234567890" />
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="App">
+                <MessageItem messageId="aaazzzddd1234567890" />
+            </div>
+        );
+    }
 }
 
 export default App;
