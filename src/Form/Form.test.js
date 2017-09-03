f9250f79e0ebd3ff21fb2fa5c441f9367b9aead1
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2017-09-03 12:47:23 +0200

    poprawka do isHex

---
+++ src/Form/Form.test.js
@@ -0,0 +1,12 @@
+import { isHex } from './Form';
+
+describe('aa', () => {
+
+    it('bb', () => {
+        expect(isHex('333')).toBe(true);
+        expect(isHex('a')).toBe(true);
+        expect(isHex('')).toBe(false);
+        expect(isHex('1a4f')).toBe(true);
+        expect(isHex('1a4fz')).toBe(false);
+    });
+})
