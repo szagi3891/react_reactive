737b1a48614c6511e1ec0d0bd1bbdeb53f296da8
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2017-09-10 11:16:11 +0200

    Dodanie przykładu z czatem

---
+++ src/Chat/Chat.js
@@ -0,0 +1,188 @@
+//@flow
+
+import * as React from 'react';
+import firebase from 'firebase';
+import BaseComponent from '../Lib/BaseComponent';
+import { Subject, ValueSubject } from '../Lib/Reactive';
+
+const config = {
+    apiKey: "AIzaSyDon__hg5Iv1zi5uvMv2V5HCSGmy6NzDGE",
+    authDomain: "rxczat.firebaseapp.com",
+    databaseURL: "https://rxczat.firebaseio.com",
+    projectId: "rxczat",
+    storageBucket: "",
+    messagingSenderId: "324635810240"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+const messages = database.ref('rxjs-demo');
+
+const nick = new ValueSubject('');
+const textarea = new ValueSubject('');
+const sending = new ValueSubject(false);
+
+type MessageItemType = {|
+    id: string,
+    nick: string,
+    message: string,
+|};
+
+const chat: ValueSubject<Array<MessageItemType>> = new ValueSubject([]);
+
+
+messages.on('child_added', function(messageItem) {
+    const currentList = chat.getValue();
+    const messageKey = messageItem.key;
+    const messageVal = messageItem.val();
+    const message = {
+        id: messageKey,
+        nick: messageVal.nick,
+        message: messageVal.message
+    }
+    currentList.push(message);
+    chat.next(currentList);
+});
+
+/*
+messages.on('child_changed', function(data) {
+    console.info('child_changed', data, data.val());
+});
+
+messages.on('child_removed', function(data) {
+    console.info('child_removed', data, data.val());
+});
+*/
+
+type PropsType = {|
+    className: string,
+|};
+
+export default class Chat extends BaseComponent<PropsType> {
+
+    click = new Subject();
+
+    _onChangeNick = (event: Object) => {
+        nick.next(event.target.value);
+    }
+
+    _onChangeTextarea = (event: Object) => {
+        textarea.next(event.target.value);
+    }
+
+    _onSend = () => {
+        this.click.next();
+    }
+
+    constructor(props: PropsType) {
+        super(props);
+
+        this.subscribe$(
+            this.click.asObservable().withLatestFrom2(
+                nick.asObservable(),
+                textarea.asObservable()
+            )
+            .do(([click, nickValue, textareaValue]) => {
+                if (nickValue.length < 1) {
+                    alert('Wpisz nick');
+                    return;
+                }
+
+                if (textareaValue.length < 1) {
+                    alert('Wpisz wiadomość');
+                    return;
+                }
+
+                sending.next(true);
+                textarea.next('');
+
+                messages.push({
+                    nick: nickValue,
+                    message: textareaValue
+                }).then((aaa) => {
+                    console.info(`Wysłano wiadomość ${textareaValue}`);
+                    sending.next(false);
+                }).catch((error: Object) => {
+                    console.error(error);
+                    sending.next(false);
+                });
+            })
+        );
+    }
+
+    render() {
+        const nickValue = this.getValue$(nick.asObservable());
+        const textareaValue = this.getValue$(textarea.asObservable());
+        const sendingValue = this.getValue$(sending.asObservable());
+
+        return (
+            <div className="Chat__wrapper">
+                <div>
+                    <div className="Chat_InputGroup">
+                        <div className="Chat_InputLabel">Nick:</div>
+                        <div className="Chat_InputBox">
+                            <input className="" value={nickValue} onChange={this._onChangeNick}/>
+                        </div>
+                    </div>
+
+                    <div className="Chat_InputGroup">
+                        <div className="Chat_InputLabel">
+                            Message:
+                        </div>
+                        <div className="Chat_InputBox">
+                            <textarea className="" value={textareaValue} onChange={this._onChangeTextarea} />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button onClick={this._onSend}>wyślij</button>
+                    </div>
+                </div>
+                { this._renderList() }
+                { sendingValue ? <div className="Chat__sending">Wysyłanie ...</div> : null }
+            </div>
+        );
+    }
+
+    _renderList() {
+        const list = this.getValue$(chat.asObservable()).concat().reverse();
+        return (
+            <div className="Chat__list App__border">
+                { list.map(item => this._renderListItem(item)) }
+            </div>
+        );
+    }
+
+    _renderListItem(item: MessageItemType) {
+        return (
+            <div className="Chat__message" key={item.id}>
+                <div className="Chat__nick">{item.nick}:</div>
+                <div>{item.message}</div>
+            </div>
+        )
+    }
+}
+
+
+/*
+this.cat = this.db.object('/cats/-KpRuC...Id')
+this.dog = this.db.object('/dogs/-KpRuD...Id')
+this.dogs = this.db.list('/dogs')
+
+https://gist.github.com/deltaepsilon/b9ac6bdb5d30b1e4203ae3b2f6e2cd07#file-rxjs-firebase-demo-js
+*/
+
+
+/*
+        const userId = "3121";
+        const name = "name111";
+        const email = "sdaasda";
+        const imageUrl = "http://wwaw.psdasda";
+
+        firebase.database().ref('users/' + userId).set({
+            username: name,
+            email: email,
+            profile_picture : imageUrl
+        });
+*/
