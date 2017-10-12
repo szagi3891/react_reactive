a967d38f582c85d335f632803231cb421cfd9e1b
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2017-10-12 22:03:23 +0200

    example

---
+++ src/Graph/ExampleComponent.jsx
@@ -0,0 +1,38 @@
+//@flow
+import * as React from 'react';
+import { BaseComponent } from 'react_reactive_value';
+import graph from 'AppGraph';
+
+type PropsType = {|
+    idThread: string
+|};
+
+class LastMessage extends BaseComponent<PropsType> {
+    render(): React.Node {
+
+        const lastMessage: MessageModel | null = this.getValue$(graph.thread.getLastMessage(this.props.idThread));
+
+        if (lastMessage) {
+            return (
+                <div>
+                    <div>Last message: lastMessage.body</div>
+                    <div>{ this._renderAuthor(lastMessage) }
+                </div>
+            )
+        }
+
+        return null;
+    }
+
+    _renderAuthor(lastMessage: MessageModel): React.Node {
+        const author: UserModel | null = this.getValue$(graph.user.get$(lastMessage.author));
+
+        if (author) {
+            return (
+                <div>Author: { author.name } </div>
+            );
+        }
+
+        return null;
+    }
+}
