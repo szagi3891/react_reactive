90dc1c52d9c3249e047639ff8316537a19935b72
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2018-02-02 05:28:20 +0100

    nowelizacja timera

--- src/MessageItem/MessageItem.js
+++ src/MessageItem/MessageItem.js
@@ -1,9 +1,12 @@
 //@flow
 import * as React from 'react';
-import { Value, ReactDecorator } from 'computed-values';
+import { Value, ReactDecorator, Computed } from 'computed-values';
 
 const counter = new Value(44);
 const counter2 = new Value(1);
+
+const counter$ = counter.asComputed();
+const counter2$ = counter2.asComputed();
 
 setInterval(() => {
     counter.setValue(counter.getValue() + 1);
@@ -13,10 +16,14 @@ setInterval(() => {
     counter2.setValue(counter2.getValue() + 1);
 }, 3000);
 
-const counter$ = counter.asComputed();
-const char$ = counter2.asComputed()
+const char$ = counter2$
     .map(count => '.'.repeat(Math.min(count, 30)));
 
+const sum$ = Computed.combine(
+    counter$,
+    counter2$,
+    (counter1, counter2) => counter1 + counter2
+);
 
 type PropsType = {|
     className: string,
@@ -29,11 +36,13 @@ export default class MessageItem extends React.Component<PropsType> {
         const { className, messageId } = this.props;
         const counter = counter$.value();
         const char = char$.value();
+        const sum = sum$.value();
 
         return (
             <div className={className}>
                 <p>{ messageId } { counter }</p>
                 <p>{char}</p>
+                <p>Suma: { sum }</p>
             </div>
         );
     }
